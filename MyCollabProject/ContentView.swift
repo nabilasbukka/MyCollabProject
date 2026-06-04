a328d51e6021aa762ca3055b3ff7d0e5d919ea09
Nabila Putri Syafrina Bukka <nabilasyafrina.business@gmail.com>
2026-06-04 10:40:08 +0700

    Initial Commit

---
+++ MyCollabProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCollabProject
+//
+//  Created by Nabila Putri Syafrina Bukka on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyCollabProject/MyCollabProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollabProjectApp.swift
+//  MyCollabProject
+//
+//  Created by Nabila Putri Syafrina Bukka on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyCollabProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
